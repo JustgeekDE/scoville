29bf076208bef5d16295953f4794da4ba874a2a9
Philip Peter <github@justgeek.de>
2015-06-11 22:41:25 +0200

    rething relations

--- src/main/scoville/eagleData.py
+++ src/main/scoville/eagleData.py
@@ -1,4 +1,10 @@
 class EagleBoard:
-  def __init__(self, fileobject):
-    self.devices = []
-    self.signals = []
+  def __init__(self):
+    self.devices = {}
+    self.deviceTypes = {}
+
+  def addDeviceType(self, device):
+    try:
+      self.deviceTypes[device.id] = device
+    except:
+      pass
