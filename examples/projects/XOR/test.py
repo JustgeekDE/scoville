1ddeafe8cd4b85bfc799c7bb9d09d347a6d9e07f
Philip Peter <github@justgeek.de>
2015-10-13 17:57:05 +0200

    More examples

--- examples/projects/XOR/test.py
+++ examples/projects/XOR/test.py
@@ -27,8 +27,41 @@ class SimulationUnitTest(unittest.TestCase):
         self.assertLess(circuit.getVoltage('AND'), 0.5)
         self.assertGreater(circuit.getVoltage('NOR'), 4.5)
         self.assertLess(circuit.getVoltage('XOR'), 0.5)
-        self.assertLess(circuit.getCurrent('Vs'), 0.1)
+        self.assertLess(circuit.getCurrent('Vs'), 0.01)
 
+    def testAHigh(self):
+        circuit = self.setupCircuit()
+
+        circuit.setSignal(SignalWithResistance("A", 5.0, 10))
+
+        circuit.run(200)
+        self.assertLess(circuit.getVoltage('AND'), 0.5)
+        self.assertLess(circuit.getVoltage('NOR'), 0.5)
+        self.assertGreater(circuit.getVoltage('XOR'), 4.5)
+        self.assertLess(circuit.getCurrent('Vs'), 0.01)
+
+    def testBHigh(self):
+        circuit = self.setupCircuit()
+
+        circuit.setSignal(SignalWithResistance("B", 5.0, 10))
+
+        circuit.run(200)
+        self.assertLess(circuit.getVoltage('AND'), 0.5)
+        self.assertLess(circuit.getVoltage('NOR'), 0.5)
+        self.assertGreater(circuit.getVoltage('XOR'), 4.5)
+        self.assertLess(circuit.getCurrent('Vs'), 0.01)
+
+    def testBothHigh(self):
+        circuit = self.setupCircuit()
+
+        circuit.setSignal(SignalWithResistance("A", 5.0, 10))
+        circuit.setSignal(SignalWithResistance("B", 5.0, 10))
+
+        circuit.run(200)
+        self.assertGreater(circuit.getVoltage('AND'), 4.5)
+        self.assertLess(circuit.getVoltage('NOR'), 0.5)
+        self.assertLess(circuit.getVoltage('XOR'), 0.5)
+        self.assertLess(circuit.getCurrent('Vs'), 0.01)
 
 if __name__ == '__main__':
     unittest.main()
