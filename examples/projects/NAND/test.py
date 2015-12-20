015caa9deb91118b4bc835c383f3e587208db804
Philip Peter <github@justgeek.de>
2015-12-20 11:31:36 -0500

    starting converter things

--- examples/projects/NAND/test.py
+++ examples/projects/NAND/test.py
@@ -45,6 +45,27 @@ class SimulationUnitTest(unittest.TestCase):
         circuit.run(200)
         self.assertLess(circuit.getVoltage('out'), 0.5)
 
+    def testShouldSwitchOnIn100ns(self):
+        circuit = Circuit.fromFile("NAND.cir")
+
+        circuit.setSignal(SignalWithResistance("inA", 5.0, 10))
+        circuit.setSignal(DelayedSignal("inB", value=0.0, delay=100, resistance=10, startValue=5.0))
+        circuit.inspectVoltage('out')
+
+        circuit.run(200, 0.1)
+        self.assertGreater(circuit.getMinVoltage('out', 100.1, 200), 4.5)
+
+    def testShouldSwitchOffIn100ns(self):
+        circuit = Circuit.fromFile("NAND.cir")
+
+        circuit.setSignal(SignalWithResistance("inA", 5.0, 10))
+        circuit.setSignal(DelayedSignal("inB", 5.0, 100, 10))
+        circuit.inspectVoltage('out')
+
+        circuit.run(200, 0.1)
+        self.assertGreater(circuit.getMinVoltage('out', 0, 100), 4.5)
+        self.assertLess(circuit.getMaxVoltage('out', 100.1, 200), 0.5)
+
     def testShouldNotUseTooMuchCurrent(self):
         circuit = Circuit.fromFile("NAND.cir")
 
@@ -66,26 +87,6 @@ class SimulationUnitTest(unittest.TestCase):
         circuit.setSignal(SignalWithResistance("inA", 5.0, 10))
         circuit.run(200)
         self.assertLess(circuit.getMaxCurrent('Vs'), 0.001)
-
-    def testShouldSwitchOffIn100ns(self):
-        circuit = Circuit.fromFile("NAND.cir")
-
-        circuit.setSignal(SignalWithResistance("inA", 5.0, 10))
-        circuit.setSignal(DelayedSignal("inB", 5.0, 100, 10))
-        circuit.inspectVoltage('out')
-
-        circuit.run(200, 0.1)
-        self.assertLess(circuit.getMaxVoltage('out', 100.1, 200), 0.5)
-
-    def testShouldSwitchOnIn100ns(self):
-        circuit = Circuit.fromFile("NAND.cir")
-
-        circuit.setSignal(SignalWithResistance("inA", 5.0, 10))
-        circuit.setSignal(DelayedSignal("inB", value=0.0, delay=100, resistance=10, startValue=5.0))
-        circuit.inspectVoltage('out')
-
-        circuit.run(200, 0.1)
-        self.assertGreater(circuit.getMinVoltage('out', 100.1, 200), 4.5)
 
 
 if __name__ == '__main__':
