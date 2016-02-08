12d921593f2697f6924e50798809cc6ea412517b
Philip Peter <github@justgeek.de>
2016-02-08 12:03:06 +0100

    Setup script

---
+++ scoville/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Scoville',
+      version='0.0',
+      description='A collection of helper scripts to unit test hardware',
+      author='Philip Peter',
+      author_email='philip.peter@justgeek.de',
+      url='https://github.com/JustgeekDE/scoville',
+      packages=[],
+     )
