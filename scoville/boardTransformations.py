014c277651868b15e1250342d08b5cc3a3bb0703
Philip Peter <github@justgeek.de>
2016-03-22 22:20:00 +0100

    Refactoring

--- scoville/boardTransformations.py
+++ scoville/boardTransformations.py
@@ -4,20 +4,26 @@ from lxml import etree
 import eagleBoard
 import genericNodeTransformations
 
-class BoardTranslation:
-  def __init__(self, translation):
-    self.coordinateTransform = genericNodeTransformations.CoordinateTranslation(translation)
+class BoardTransformation:
 
-  def transform(self, board):
-
-    originalXMLString = board.toString()
-    xml = etree.fromstring(originalXMLString, etree.XMLParser(remove_blank_text=True))
-
+  def getTransformableElements(self,xml):
     elements = []
     elements.extend(xml.findall('./drawing/board/plain/*'))
     elements.extend(xml.findall('./drawing/board/signals/signal/*'))
     elements.extend(xml.findall('./drawing/board/elements/*'))
 
+    return elements
+
+
+class BoardTranslation(BoardTransformation):
+  def __init__(self, translation):
+    self.coordinateTransform = genericNodeTransformations.CoordinateTranslation(translation)
+
+  def transform(self, board):
+    originalXMLString = board.toString()
+    xml = etree.fromstring(originalXMLString, etree.XMLParser(remove_blank_text=True))
+
+    elements = self.getTransformableElements(xml)
     for element in elements:
       self.coordinateTransform.translate(element)
 
