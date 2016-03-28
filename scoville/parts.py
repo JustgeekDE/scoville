de050aa90091ac1a92714e39b4ee3f0462d91b2b
Philip Peter <github@justgeek.de>
2016-03-28 19:35:15 +0200

    Changed voltage source resitance to 0

--- scoville/parts.py
+++ scoville/parts.py
@@ -4,10 +4,11 @@ class GenericVoltageSource:
     self.netHigh = netHigh
     self.netGND = netGND
     self.voltage = voltage
+    self.resistance = 0
 
   def getSpiceDefinition(self):
     voltageSource = "Vmock{0} {0} GND dc {1}V ac 0V\n".format(self.netHigh, self.voltage)
-    resistor = "R{0} {1} GND 1".format(self.name, self.netGND)
+    resistor = "R{0} {1} GND {2}".format(self.name, self.netGND, self.resistance)
     return voltageSource + resistor
 
 
